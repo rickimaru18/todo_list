937c5bd39f5fec465381b8e0b0d0a05f13b27253
Rick Krystianne Lim <rickimaru18@gmail.com>
2022-10-08 22:27:50 +0800

    chore: generate TodoUsecase mock

--- test/mocks.dart
+++ test/mocks.dart
@@ -1,6 +1,7 @@
 import 'package:mockito/annotations.dart';
 import 'package:todo_list/src/data/datasources/todo_datasource.dart';
 import 'package:todo_list/src/data/repositories/todo_repository.dart';
+import 'package:todo_list/src/domain/usecases/todo_usecase.dart';
 
 export 'mocks.mocks.dart';
 
@@ -8,6 +9,7 @@ export 'mocks.mocks.dart';
   <Type>[
     TodoDataSource,
     TodoRepository,
+    TodoUsecase,
   ],
 )
 class Mocks {}
