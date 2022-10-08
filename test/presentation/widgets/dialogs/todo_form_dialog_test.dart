c3331781fb4a5ee02813e71ba15212faae04d48a
Rick Krystianne Lim <rickimaru18@gmail.com>
2022-10-08 22:28:49 +0800

    test: make "inputFormWithChecks" global in "todo_form_dialog_test.dart"
    Also rename it to "inputTodoFormDialogWithChecks".

--- test/presentation/widgets/dialogs/todo_form_dialog_test.dart
+++ test/presentation/widgets/dialogs/todo_form_dialog_test.dart
@@ -101,78 +101,6 @@ void main() {
   });
 
   group('[Event checks]', () {
-    // Input form fields with [expect] checks.
-    Future<void> inputFormWithChecks(WidgetTester tester) async {
-      // Input task name
-      await tester.enterText(
-        find.widgetWithText(TextFormField, 'Task name'),
-        'Sample task',
-      );
-      expect(find.widgetWithText(TextFormField, 'Sample task'), findsOneWidget);
-
-      // TODO: priority dropdown change
-      // Tap priority dropdown
-      // expect(
-      //   find.widgetWithText(DropdownButtonFormField<Priority>, 'none'),
-      //   findsOneWidget,
-      // );
-      // await tester.tap(find.byType(DropdownButtonFormField<Priority>));
-      // await tester.pumpAndSettle();
-
-      // // Priority dropdown items
-      // expect(
-      //   find.widgetWithText(DropdownMenuItem<Priority>, 'none'),
-      //   // 2 because the default selected is "none"
-      //   findsNWidgets(2),
-      // );
-      // expect(
-      //   find.widgetWithText(DropdownMenuItem<Priority>, 'low'),
-      //   // Not sure why it finds 2
-      //   findsNWidgets(2),
-      // );
-      // expect(
-      //   find.widgetWithText(DropdownMenuItem<Priority>, 'medium'),
-      //   // Not sure why it finds 2
-      //   findsNWidgets(2),
-      // );
-      // expect(
-      //   find.widgetWithText(DropdownMenuItem<Priority>, 'high'),
-      //   // Not sure why it finds 2
-      //   findsNWidgets(2),
-      // );
-
-      // await tester.tap(find.text('low').last);
-      // await tester.pumpAndSettle();
-
-      // expect(
-      //   find.widgetWithText(DropdownButtonFormField<Priority>, 'none'),
-      //   findsNothing,
-      // );
-      // expect(
-      //   find.widgetWithText(DropdownButtonFormField<Priority>, 'low'),
-      //   findsOneWidget,
-      // );
-
-      // Tap due date icon button
-      await tester.tap(find.widgetWithIcon(IconButton, Icons.date_range));
-      await tester.pumpAndSettle();
-
-      expect(find.byType(DatePickerDialog), findsOneWidget);
-
-      await tester.tap(find.text('OK'));
-      await tester.pumpAndSettle();
-
-      expect(find.byType(DatePickerDialog), findsNothing);
-
-      expect(find.text('Due date'), findsOneWidget);
-      expect(find.widgetWithIcon(IconButton, Icons.date_range), findsOneWidget);
-      expect(
-        find.text(DateFormat.yMMMMd().format(DateTime.now())),
-        findsOneWidget,
-      );
-      expect(find.widgetWithText(TextButton, 'Clear'), findsOneWidget);
-    }
-
     testWidgets('New TODO inputs + submit', (WidgetTester tester) async {
       Todo? res;
 
@@ -180,7 +108,7 @@ void main() {
 
       expect(find.byType(AlertDialog), findsOneWidget);
 
-      await inputFormWithChecks(tester);
+      await inputTodoFormDialogWithChecks(tester);
 
       // Submit
       await tester.tap(find.widgetWithIcon(IconButton, Icons.check));
@@ -200,7 +128,7 @@ void main() {
 
       expect(find.byType(AlertDialog), findsOneWidget);
 
-      await inputFormWithChecks(tester);
+      await inputTodoFormDialogWithChecks(tester);
 
       // Submit
       await tester.tap(find.widgetWithIcon(IconButton, Icons.close));
@@ -217,7 +145,7 @@ void main() {
 
         expect(find.byType(AlertDialog), findsOneWidget);
 
-        await inputFormWithChecks(tester);
+        await inputTodoFormDialogWithChecks(tester);
 
         // Clear due date
         await tester.tap(find.widgetWithText(TextButton, 'Clear'));
@@ -273,7 +201,7 @@ void main() {
 
       expect(find.byType(AlertDialog), findsOneWidget);
 
-      await inputFormWithChecks(tester);
+      await inputTodoFormDialogWithChecks(tester);
 
       // Submit
       await tester.tap(find.widgetWithIcon(IconButton, Icons.check));
@@ -303,7 +231,7 @@ void main() {
 
       expect(find.byType(AlertDialog), findsOneWidget);
 
-      await inputFormWithChecks(tester);
+      await inputTodoFormDialogWithChecks(tester);
 
       // Submit
       await tester.tap(find.widgetWithIcon(IconButton, Icons.close));
@@ -313,4 +241,77 @@ void main() {
       expect(res, null);
     });
   });
+}
+
+//------------------------------------------------------------------------------
+/// Input [showTodoFormDialog] form fields with [expect] checks.
+Future<void> inputTodoFormDialogWithChecks(WidgetTester tester) async {
+  // Input task name
+  await tester.enterText(
+    find.widgetWithText(TextFormField, 'Task name'),
+    'Sample task',
+  );
+  expect(find.widgetWithText(TextFormField, 'Sample task'), findsOneWidget);
+
+  // TODO: priority dropdown change
+  // Tap priority dropdown
+  // expect(
+  //   find.widgetWithText(DropdownButtonFormField<Priority>, 'none'),
+  //   findsOneWidget,
+  // );
+  // await tester.tap(find.byType(DropdownButtonFormField<Priority>));
+  // await tester.pumpAndSettle();
+
+  // // Priority dropdown items
+  // expect(
+  //   find.widgetWithText(DropdownMenuItem<Priority>, 'none'),
+  //   // 2 because the default selected is "none"
+  //   findsNWidgets(2),
+  // );
+  // expect(
+  //   find.widgetWithText(DropdownMenuItem<Priority>, 'low'),
+  //   // Not sure why it finds 2
+  //   findsNWidgets(2),
+  // );
+  // expect(
+  //   find.widgetWithText(DropdownMenuItem<Priority>, 'medium'),
+  //   // Not sure why it finds 2
+  //   findsNWidgets(2),
+  // );
+  // expect(
+  //   find.widgetWithText(DropdownMenuItem<Priority>, 'high'),
+  //   // Not sure why it finds 2
+  //   findsNWidgets(2),
+  // );
+
+  // await tester.tap(find.text('low').last);
+  // await tester.pumpAndSettle();
+
+  // expect(
+  //   find.widgetWithText(DropdownButtonFormField<Priority>, 'none'),
+  //   findsNothing,
+  // );
+  // expect(
+  //   find.widgetWithText(DropdownButtonFormField<Priority>, 'low'),
+  //   findsOneWidget,
+  // );
+
+  // Tap due date icon button
+  await tester.tap(find.widgetWithIcon(IconButton, Icons.date_range));
+  await tester.pumpAndSettle();
+
+  expect(find.byType(DatePickerDialog), findsOneWidget);
+
+  await tester.tap(find.text('OK'));
+  await tester.pumpAndSettle();
+
+  expect(find.byType(DatePickerDialog), findsNothing);
+
+  expect(find.text('Due date'), findsOneWidget);
+  expect(find.widgetWithIcon(IconButton, Icons.date_range), findsOneWidget);
+  expect(
+    find.text(DateFormat.yMMMMd().format(DateTime.now())),
+    findsOneWidget,
+  );
+  expect(find.widgetWithText(TextButton, 'Clear'), findsOneWidget);
 }
