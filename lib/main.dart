1502f67c18ed234d22c134944ece6fb20b1500f4
Rick Krystianne Lim <rickimaru18@gmail.com>
2022-10-08 14:19:47 +0800

    feat: add TodoDataSource which is a local data source for Todo
    Also initialize in "main".

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 
+import 'src/data/datasources/todo_datasource.dart';
 import 'src/presentation/pages/page_router.dart';
 
-void main() {
+Future<void> main() async {
+  await TodoDataSource().init();
+
   runApp(const App());
 }
 
